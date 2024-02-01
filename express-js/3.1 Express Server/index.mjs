57242bdaf671fac0a9237891275a8f1de625180c
gustedeveloper <219.guste@gmail.com>
2024-02-01 12:11:44 +0100

    Installing nodemon

--- express-js/3.1 Express Server/index.mjs
+++ express-js/3.1 Express Server/index.mjs
@@ -1,13 +1,15 @@
 //How to reach port 3000: localhost:3000
 
 //Check which ports on our computer are listening for interactions: netstat -ano | findstr "LISTENING"
+//nodemon restarts the server when changes are made
+
 
 import express from "express";
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.send("<h1>Hello!</h1>");
 })
 
 app.listen(port, () => {
