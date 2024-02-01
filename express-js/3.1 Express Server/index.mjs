3bc03cc51470215e75b94ff87829279e354f921f
gustedeveloper <219.guste@gmail.com>
2024-02-01 12:25:56 +0100

    Expanded Express app with /contact and /about routes, each providing specific HTML responses

--- express-js/3.1 Express Server/index.mjs
+++ express-js/3.1 Express Server/index.mjs
@@ -9,7 +9,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello!</h1>");
+    res.send("<h1>Welcome to my blog!</h1>");
+})
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact me</h1><p>On Instagram @mwgbt</p><p>Via Gmail: 219_guste@gmail.com</p>")
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About me</h1><p>My name is Guste and I'm a Full Stack Javascript Web Developer</p>")
 })
 
 app.listen(port, () => {
