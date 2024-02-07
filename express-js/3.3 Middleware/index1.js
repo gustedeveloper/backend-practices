7c76965b7572f2d829eef0b22e5ddfee4085734b
gustedeveloper <219.guste@gmail.com>
2024-02-07 18:38:08 +0100

    Added custom logging middleware to capture request details

--- express-js/3.3 Middleware/index1.js
+++ express-js/3.3 Middleware/index1.js
@@ -1,21 +1,20 @@
 import express from "express";
-import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-})
+app.use(express.urlencoded({extended: true}));  //Middleware
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- express-js/3.3 Middleware/index3.js
+++ express-js/3.3 Middleware/index3.js
@@ -3,6 +3,17 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl || req.url;
+  const logMessage = timestamp + method + url;
+
+  console.log(logMessage);
+
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
